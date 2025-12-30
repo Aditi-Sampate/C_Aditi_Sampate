e0855235d92560e3336515ba1e1bbc322ce1eba3
Aditi-Sampate <aditisampate@gmail.com>
2025-12-30 21:59:49 +0530

    Check the Number is greater using simple if

---
+++ Conditional_Statements/if statement/if_greater.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+    void display(int a)
+    {
+        if(a>500)
+        {
+        printf("A is greater than 500 ",a);
+        }
+    }
+
+    int main()
+    {
+        int ino;
+
+        printf("Enter the  any number=");
+        scanf("%d",&ino);
+
+        display(ino);
+        return 0;
+
+    }
