aca6cf5852e39d588195762eb1181ecf47be4668
Aditi-Sampate <aditisampate@gmail.com>
2026-01-01 22:48:52 +0530

    Implemented the Simple Check Num Using The Nested If-else

---
+++ Conditional_Statements/nested if else/check_num.c
@@ -0,0 +1,42 @@
+/* Write a program of greater or smaller by using nested if else statement */
+
+#include<stdio.h>
+#include<conio.h>
+
+void check_number(int ino)
+{
+    if(ino<100)
+    {
+        if(ino>50)
+        {
+            printf("ino is greater than 50");
+        }
+        else
+        {
+            printf("ino is less than 50");
+        }
+        
+    }
+    else
+    {
+        if(ino>100)
+        {
+            printf("ino is greater than 100");
+        }
+        else
+        {
+            printf("ino is smaller than 100");
+        }
+    }
+}
+
+int main()
+{
+    int ino1;
+
+    printf("Enter value for ino = ");
+    scanf("%d",&ino1);
+
+    check_number(ino1);
+    return 0;
+}
