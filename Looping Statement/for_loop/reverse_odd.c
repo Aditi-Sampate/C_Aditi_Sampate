6846ef2acc32a7c546dcaf00521ef2e3de4af700
Aditi-Sampate <aditisampate@gmail.com>
2026-01-05 16:56:02 +0530

    Print reverse of odd numbers using for loop

---
+++ Looping Statement/for_loop/reverse_odd.c
@@ -0,0 +1,34 @@
+/*
+  Write a program to print the reverse of odd number using for loop.
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+void reverse(int ino,int icnt)
+{
+    for(icnt=icnt;icnt>=ino;icnt--)
+    {
+        if(icnt%2!=0)
+        {
+            printf("%d\n",icnt);
+        }
+        
+    }
+
+}
+
+int main()
+{
+    int ino1;
+    int icnt1;
+
+    printf("Enter the first number= ");
+    scanf("%d",&ino1);
+    printf("Enter the last number= ");
+    scanf("%d",&icnt1);
+
+    reverse(ino1,icnt1);
+    return 0;
+
+}
