b46ae4b5051cd6b9d8ea88331fda71f6a7d9df8d
Aditi-Sampate <aditisampate@gmail.com>
2026-01-13 21:54:52 +0530

    Program Demonstrating The Use Of a Do-While Loop To Print The Even Number.

---
+++ Looping Statement/do_while_loop/even.c
@@ -0,0 +1,37 @@
+/*
+  Write a program to print the even number using do while loop.
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+void even(int ino,int icnt)
+{
+    do
+    {
+        if(ino%2==0)
+        {
+            printf("%d\n",ino);
+        }
+        
+        ino++;
+    }
+
+    while (ino<=icnt);
+
+}
+
+int main()
+{
+    int ino1;
+    int icnt1;
+
+    printf("Enter the first number = ");
+    scanf("%d",&ino1);
+
+    printf("Enter the last number = ");
+    scanf("%d",&icnt1);
+
+    even(ino1,icnt1);
+    return 0;
+}
